247514505164e75e1b02d7b7ad943b27ab267ea2
songww <sww4718168@gmail.com>
2022-06-09 12:18:15 +0800

    Fix init.lua

--- nvim/lua/custom/init.lua
+++ nvim/lua/custom/init.lua
@@ -3,12 +3,12 @@ require("nvim-treesitter.install").command_extra_args = {
   curl = { "--proxy", "http://127.0.0.1:1087" },
 }
 
+vim.o.guifont = "Monaco:h13"
+
+vim.o.ruler = true
 vim.o.clipboard = "unnamedplus"
 vim.o.directory = "."
-vim.o.shortmess:append("c")
-vim.o.ruler = true
-
-vim.o.guifont = "Cascadia Code:h12"
+vim.opt.shortmess:append({ c = true })
 
 -- AUTOCMDS
 require("custom.autocmds")
