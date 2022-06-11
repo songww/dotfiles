a6b168371344e8ed56cb0c416ee4a997876bbde1
songww <sww4718168@gmail.com>
2022-06-11 19:29:25 +0800

    disable trouble's auto close to prevent shake

--- nvim/lua/custom/plugins/configs/trouble.lua
+++ nvim/lua/custom/plugins/configs/trouble.lua
@@ -26,7 +26,7 @@ M.setup = function()
     },
     indent_lines = false, -- add an indent guide below the fold icons
     auto_open = true, -- automatically open the list when you have diagnostics
-    auto_close = true, -- automatically close the list when you have no diagnostics
+    auto_close = false, -- automatically close the list when you have no diagnostics
     use_diagnostic_signs = true -- enabling this will use the signs defined in your lsp client
   })
 
